5bd5cf499aedd09ebe4d7b4205e2d9d2d67a99bc
StaviRare <StaviRare@gmail.com>
2024-06-28 11:34:01 +0300

    Updated deprecated code.

--- source/app/android/java/EnginePlayer.java
+++ source/app/android/java/EnginePlayer.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class EnginePlayer extends SurfaceView implements SurfaceHolder.Callback
 {
@@ -29,7 +30,7 @@ public class EnginePlayer extends SurfaceView implements SurfaceHolder.Callback
     private final Activity activity;
     private SurfaceHolder surfaceHolder;
     private boolean isInitialized = false;
-    private final Handler handler = new Handler();
+    private final Handler handler = new Handler(Looper.getMainLooper());
     private final Runnable renderTask = new Runnable()
     {
         @Override
@@ -94,8 +95,14 @@ public class EnginePlayer extends SurfaceView implements SurfaceHolder.Callback
 
     private void setFullScreenMode()
     {
-        activity.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        WindowCompat.setDecorFitsSystemWindows(activity.getWindow(), false);
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(activity.getWindow(), activity.getWindow().getDecorView());
+
+        if (insetsController != null)
+        {
+            insetsController.hide(WindowInsetsCompat.Type.systemBars());
+            insetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
     }
 
     private void hideActionBar()
@@ -108,15 +115,13 @@ public class EnginePlayer extends SurfaceView implements SurfaceHolder.Callback
 
     private void setImmersiveMode()
     {
-        activity.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE |
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(activity.getWindow(), activity.getWindow().getDecorView());
+
+        if (insetsController != null)
+        {
+            insetsController.hide(WindowInsetsCompat.Type.systemBars());
+            insetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
     }
 
     private void setupSurfaceView()
@@ -155,7 +160,6 @@ public class EnginePlayer extends SurfaceView implements SurfaceHolder.Callback
     {
         handler.removeCallbacks(renderTask);
     }
-
 
     // Native methods
     private native void nativeSetSurface(Surface surface);
